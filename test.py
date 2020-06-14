bb8a33d386106ce8a0b7fa3397146088fdb1530f
vosr17 <43559631+vosr17@users.noreply.github.com>
2020-06-14 19:09:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,56 +1,24 @@
-from main import substraction
 import main
 import unittest
 
+class MyTestCase(unittest.TestCase):
 
-class TestOnlineCalculator(unittest.TestCase):
-    """Testing features of substraction in online calculator"""
-
-    def setUp(self):
-        """Sets up the app for testing"""
-        main.app.testing = True
-        self.app = main.app.test_client()
-
-    def test_substraction(self):
-        """Tests page with /sub route, testing substraction feature of the calculator,
-        right now all types of numbers being tested"""
-
-        # integer numbers testing
-        response_data = self.app.get('/sub?A=34&B=30')
-        self.assertEqual(b'4 \n', response_data.data)
-
-        # rational numbers testing
-        response_data = self.app.get('/sub?A=2/3&B=3/7')
-        self.assertEqual(b'0.238095238095238', response_data.data)
-
-        # when both A and B are both floats
-        response_data = self.app.get('/sub?A=5.4&B=3.4')
-        self.assertEqual(b'2 \n', response_data.data)
-
-        # when A is an int and B is float
-        response_data = self.app.get('/sub?A=8&B=1.234')
-        self.assertEqual(b'6.766', response_data.data)
-
-        # when A is a float and B is an int
-        response_data = self.app.get('/sub?A=-5.35&B=2')
-        self.assertEqual(b'-7.35', response_data.data)
-
-        # when A is a fraction and B is an int
-        response_data = self.app.get('/sub?A=4/5&B=3')
-        self.assertEqual(b'-2.2', response_data.data)
-
-        # when A is an int and B is a fraction
-        response_data = self.app.get('/sub?A=7&B=5/6')
-        self.assertEqual(b'6.166666666666667', response_data.data)
-
-        # corner cases testing
-        # when A = x/0 where x belongs to integer
-        response_data = self.app.get('/sub?A=-1/0&B=7/9')
-        self.assertEqual(b"A's denominator should not be zero! \n", response_data.data)
-
-        # when B = x/0 where x belongs to integer
-        response_data = self.app.get('/sub?A=-4&B=1000/0')
-        self.assertEqual(b"B's denominator should not be zero! \n", response_data.data)
+        def setUp(self):
+            main.app.testing = True
+            self.app = main.app.test_client()
+        def test_divint(self):
+            rv =  self.app.get('/division?A=7&B=6')
+            self.assertEqual(b'1.16666666666667', rv.data)
+        def test_divfloat(self):
+            rv =  self.app.get('/division?A=7.5&B=2.5')
+            self.assertEqual(b'3 \n', rv.data)
+        def test_divfrac(self):
+            rv =  self.app.get('/division?A=7/5&B=3/5')
+            self.assertEqual(b'2.33333333333333', rv.data)
+        def test_divneg(self):
+            rv =  self.app.get('/division?A=7.5&B=-2.5')
+            self.assertEqual(b'-3 \n', rv.data)
+                 
 
 if __name__ == '__main__':
     unittest.main()
